3cd7920b2211fed806c193efa7faccebe06dfd8a
Alexander Jackson <alexanderjackson@protonmail.com>
2024-08-18 09:44:35 +0100

    feat: get a basic HTTP server working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let router = Router::new().route("/", get(handler));
+    let listener = TcpListener::bind("localhost:8000").await?;
+
+    axum::serve(listener, router.into_make_service()).await?;
+
+    Ok(())
+}
+
+async fn handler() -> &'static str {
+    "Hello World!"
 }
