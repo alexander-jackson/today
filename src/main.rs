70d9f6fa7b5b62f0c4a7e31a09d2042db6d34227
Alexander Jackson <alexanderjackson@protonmail.com>
2024-08-18 14:17:51 +0100

    feat: add some basic tests

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use axum::routing::get;
 use axum::Router;
 use tokio::net::TcpListener;
 
+fn build_router() -> Router {
+    Router::new().route("/", get(handler))
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let router = Router::new().route("/", get(handler));
+    let router = build_router();
     let listener = TcpListener::bind("localhost:8000").await?;
 
     axum::serve(listener, router.into_make_service()).await?;
@@ -15,3 +19,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 async fn handler() -> &'static str {
     "Hello World!"
 }
+
+#[cfg(test)]
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,46 @@
+use axum::body::Body;
+use axum::http::{Request, StatusCode};
+use axum::response::Response;
+use http_body_util::BodyExt;
+use tower_util::ServiceExt;
+
+use crate::build_router;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+async fn get_body(response: Response) -> Result<String> {
+    let body = response.into_body().collect().await?.to_bytes();
+    let message = String::from_utf8(body.to_vec())?;
+
+    Ok(message)
+}
+
+#[tokio::test]
+async fn it_returns_a_static_response() -> Result<()> {
+    let router = build_router();
+    let request = Request::builder().uri("/").body(Body::empty())?;
+
+    let response = router.oneshot(request).await?;
+    let status = response.status();
+    let body = get_body(response).await?;
+
+    assert_eq!(status, StatusCode::OK);
+    assert_eq!(body, "Hello World!");
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn invalid_requests_get_404s() -> Result<()> {
+    let router = build_router();
+    let request = Request::builder()
+        .uri("/unknown-path")
+        .body(Body::empty())?;
+
+    let response = router.oneshot(request).await?;
+    let status = response.status();
+
+    assert_eq!(status, StatusCode::NOT_FOUND);
+
+    Ok(())
+}
