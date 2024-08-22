04bf2b27d2f48d2740b201a589dc75e15ae38e66
Alexander Jackson <alex.jackson@starlingbank.com>
2024-08-22 15:17:17 +0100

    feat: add deleted item state (#10)
    Sometimes items are added with typos, which cannot be easily corrected.
    Instead of mutating the `item` table we can just insert a deletion event
    (which will be fully connected up in the future).
    
    This change:
    * Adds proper item states
    * Updates all the queries to handle them
    * Adds a test to make sure we don't return deleted items

--- src/persistence/mod.rs
+++ src/persistence/mod.rs
@@ -1,31 +1,64 @@
 use chrono::{NaiveDate, NaiveDateTime, Utc};
 use color_eyre::eyre::Result;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
 use uuid::Uuid;
 
 pub mod bootstrap;
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum ItemState {
+    Checked,
+    Unchecked,
+    Deleted,
+}
+
+impl ItemState {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Self::Checked => "Checked",
+            Self::Unchecked => "Unchecked",
+            Self::Deleted => "Deleted",
+        }
+    }
+}
+
+impl From<String> for ItemState {
+    fn from(value: String) -> Self {
+        match value.as_str() {
+            "Checked" => Self::Checked,
+            "Unchecked" => Self::Unchecked,
+            "Deleted" => Self::Deleted,
+            _ => panic!("invalid argument {value}"),
+        }
+    }
+}
+
 #[derive(Serialize)]
 pub struct Item {
     item_uid: Uuid,
     content: String,
-    pub state: bool,
+    pub state: ItemState,
 }
 
 pub async fn select_items(pool: &PgPool, date: NaiveDate) -> Result<Vec<Item>> {
     let items = sqlx::query_as!(
         Item,
         r#"
-            SELECT DISTINCT ON (i.id)
-                i.item_uid,
-                i.content,
-                CASE WHEN iet.name = 'Checked' THEN true ELSE false END AS "state!"
-            FROM item i
-            JOIN item_event ie ON i.id = ie.item_id
-            JOIN item_event_type iet ON iet.id = ie.event_type_id
-            WHERE i.created_at::date = $1
-            ORDER BY i.id, i.created_at, ie.occurred_at DESC
+            WITH items_with_states AS (
+                SELECT DISTINCT ON (i.id)
+                    i.item_uid,
+                    i.content,
+                    iet.name AS state
+                FROM item i
+                JOIN item_event ie ON i.id = ie.item_id
+                JOIN item_event_type iet ON iet.id = ie.event_type_id
+                WHERE i.created_at::date = $1
+                ORDER BY i.id, i.created_at, ie.occurred_at DESC
+            )
+            SELECT item_uid, content, state
+            FROM items_with_states
+            WHERE state != 'Deleted'
         "#,
         date,
     )
@@ -75,10 +108,8 @@ pub async fn create_item(
     Ok(())
 }
 
-pub async fn update_item(pool: &PgPool, item_uid: Uuid, state: bool) -> Result<()> {
+pub async fn update_item(pool: &PgPool, item_uid: Uuid, state: ItemState) -> Result<()> {
     let mut tx = pool.begin().await?;
-    let state = if state { "Checked" } else { "Unchecked" };
-
     let now = Utc::now().naive_local();
 
     sqlx::query!(
@@ -91,7 +122,7 @@ pub async fn update_item(pool: &PgPool, item_uid: Uuid, state: bool) -> Result<(
             )
         "#,
         item_uid,
-        state,
+        state.as_str(),
         now,
     )
     .execute(&mut *tx)
