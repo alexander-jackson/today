c9c2b3ba5865def961ee99f4448057b063f5a8a5
Alexander Jackson <alex.jackson@starlingbank.com>
2024-09-26 17:20:58 +0100

    refactor: tidy up tests
    The tests are a bit of a mess at the moment and are quite verbose. Let's
    work on getting them a bit easier to work with so that we can write more
    complex ones in the future.
    
    This change:
    * Refactors a bunch of structures out of them

--- src/tests.rs
+++ src/tests.rs
@@ -16,6 +16,52 @@ const FORM_MIME_TYPE: &str = "application/x-www-form-urlencoded";
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
+struct AccountDetails {
+    email_address: &'static str,
+    password: &'static str,
+}
+
+impl Default for AccountDetails {
+    fn default() -> Self {
+        Self {
+            email_address: "test@test.com",
+            password: "test",
+        }
+    }
+}
+
+struct AccountClient<'a> {
+    router: &'a mut Router,
+    cookie: String,
+}
+
+impl<'a> AccountClient<'a> {
+    async fn request_index(&mut self) -> Result<Response> {
+        let request = Request::builder()
+            .method(Method::GET)
+            .uri("/")
+            .header(COOKIE, &self.cookie)
+            .body(Body::empty())?;
+
+        let response = self.router.call(request).await?;
+
+        Ok(response)
+    }
+
+    async fn create_item(&mut self) -> Result<Response> {
+        let request = Request::builder()
+            .method(Method::POST)
+            .uri("/add")
+            .header(CONTENT_TYPE, FORM_MIME_TYPE)
+            .header(COOKIE, &self.cookie)
+            .body(Body::from("content=Task"))?;
+
+        let response = self.router.call(request).await?;
+
+        Ok(response)
+    }
+}
+
 fn build_router(pool: PgPool) -> Result<Router> {
     let template_engine = TemplateEngine::new()?;
     let cookie_key = Key::generate();
@@ -44,28 +90,40 @@ fn get_response_header<'a, K: AsHeaderName>(response: &'a Response, header: K) -
     response.headers().get(header).and_then(|h| h.to_str().ok())
 }
 
-async fn create_account(router: &mut Router) -> Result<()> {
+async fn create_account(
+    router: &mut Router,
+    AccountDetails {
+        email_address,
+        password,
+    }: &AccountDetails,
+) -> Result<()> {
     let request = Request::builder()
         .method(Method::PUT)
         .uri("/register")
         .header(CONTENT_TYPE, FORM_MIME_TYPE)
-        .body(Body::from(
-            "email_address=test@test.com&raw_password=password",
-        ))?;
+        .body(Body::from(format!(
+            "email_address={email_address}@test.com&raw_password={password}"
+        )))?;
 
     router.call(request).await?;
 
     Ok(())
 }
 
-async fn login_to_account(router: &mut Router) -> Result<(Response, String)> {
+async fn login_to_account<'a>(
+    router: &'a mut Router,
+    AccountDetails {
+        email_address,
+        password,
+    }: &AccountDetails,
+) -> Result<(Response, AccountClient<'a>)> {
     let request = Request::builder()
         .method(Method::POST)
         .uri("/login")
         .header(CONTENT_TYPE, FORM_MIME_TYPE)
-        .body(Body::from(
-            "email_address=test@test.com&raw_password=password",
-        ))?;
+        .body(Body::from(format!(
+            "email_address={email_address}@test.com&raw_password={password}",
+        )))?;
 
     let response = router.call(request).await?;
     let cookie = response
@@ -75,7 +133,7 @@ async fn login_to_account(router: &mut Router) -> Result<(Response, String)> {
         .to_str()?
         .to_owned();
 
-    Ok((response, cookie))
+    Ok((response, AccountClient { cookie, router }))
 }
 
 #[sqlx::test]
@@ -96,36 +154,24 @@ async fn invalid_requests_get_404s(pool: PgPool) -> Result<()> {
 #[sqlx::test]
 async fn can_add_items(pool: PgPool) -> Result<()> {
     let mut router = build_router(pool)?;
+    let account_details = AccountDetails::default();
 
     // Create an account
-    create_account(&mut router).await?;
+    create_account(&mut router, &account_details).await?;
 
     // Log in to the account to get a token
-    let (response, cookie) = login_to_account(&mut router).await?;
+    let (response, mut client) = login_to_account(&mut router, &account_details).await?;
 
     assert_eq!(response.status(), StatusCode::FOUND);
     assert_eq!(get_response_header(&response, LOCATION), Some("/"));
 
-    let request = Request::builder()
-        .method(Method::POST)
-        .uri("/add")
-        .header(CONTENT_TYPE, FORM_MIME_TYPE)
-        .header(COOKIE, &cookie)
-        .body(Body::from("content=Task"))?;
-
-    let response = router.call(request).await?;
+    let response = client.create_item().await?;
 
     // Get redirected to the index page
     assert_eq!(response.status(), StatusCode::FOUND);
     assert_eq!(get_response_header(&response, LOCATION), Some("/"));
 
-    let request = Request::builder()
-        .method(Method::GET)
-        .uri("/")
-        .header(COOKIE, &cookie)
-        .body(Body::empty())?;
-
-    let response = router.call(request).await?;
+    let response = client.request_index().await?;
 
     assert_eq!(response.status(), StatusCode::OK);
     assert_eq!(
